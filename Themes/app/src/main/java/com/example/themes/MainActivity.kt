11b7fe7d2574523fabf22ded419ab8c01d6cd18a
ShubhamShinde96 <shindesshubham96@gmail.com>
2024-01-05 01:08:03 +0530

    Started project Themes

---
+++ Themes/app/src/main/java/com/example/themes/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.themes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.themes.ui.theme.ThemesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App()
+        }
+    }
+}
+
+@Composable
+fun App() {
+    var theme = remember {
+        mutableStateOf(false)
+    }
+    ThemesTheme(theme.value) { // Theme name created as per our app name, our app name is theme
+        // A surface container using the 'background' color from the theme.
+        // We have to check Theme.kt
+        // Check each file in ui.theme package for more details
+
+        Column(
+            modifier = Modifier.background(MaterialTheme
+                .colorScheme.background)
+        ) {
+            Text(
+                text = "Cheezy Code",
+                style = MaterialTheme.typography.titleLarge // This we have re defined in ui.theme/Type.kt
+            )
+            Button(onClick = {
+                theme.value = !theme.value // this will assign opposite value always, this will help us change the theme.
+            }) {
+                Text(text = "Change Theme")
+            }
+            // So here we're changin theme based on this button click.
+        }
+
+    }
+}
